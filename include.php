951caf87e938ab92787b8f0b800233561730485e
schedler <schedler@physik.uni-bonn.de>
2019-03-04 09:50:36 +0100

    + templates

---
+++ include.php
@@ -0,0 +1,9 @@
+<?php
+
+include "file1.php";
+include "file2.php";
+.
+.
+.
+
+?>
---
+++ sample.php
@@ -0,0 +1,12 @@
+<?php
+include "include.php" // includes all local libraries
+?>
+
+<html>
+  <head>
+    <link rel="stylesheet" type="text/css" href="meldeliste.css">
+
+  </head
+  <body>
+  </body>
+</html
