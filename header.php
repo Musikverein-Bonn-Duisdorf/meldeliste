f0b25c07ac25ed857d308c32632e566d1d8e7bfc
schedler <schedler@physik.uni-bonn.de>
2019-03-05 18:52:55 +0100

    fix

--- header.php
+++ header.php
@@ -2,24 +2,17 @@
 <html lang="de">
   <head>
       <link rel="stylesheet" href="MVD.css">
-<?php
-      try {
+      <?php
           include 'include.php';
-      }
-      catch(Exception $e) {
-          echo 'Exception: ',  $e->getMessage(), "\n";
-      }
-
-try {
-    $conn=mysqli_connect($config->server, $config->user, $config->password) or die (mysqli_error($conn));
-    mysqli_select_db($conn, $config->database) or die(mysqli_error($conn));
-}
-catch(Exception $e) {
-    echo 'Exception: ',  $e->getMessage(), "\n";
-}          
-?>
-<meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo $commonStrings->WebSiteName; ?></title>
+      ?>
+      <!-- successfully included php libraries -->
+      <?php
+          $conn=mysqli_connect($config['server'], $config['user'], $config['password']) or die (mysqli_error($conn));
+          mysqli_select_db($conn, $config['database']) or die(mysqli_error($conn));
+      ?>
+      <!-- successfully connected to MySQL database -->
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title><?php echo $commonStrings['WebSiteName']; ?></title>
   </head>
   <body>
