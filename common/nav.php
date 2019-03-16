d3d246de4b284e653897b522cd9a7b5a7c6cee6d
Manuel Schedler <schedler@physik.uni-bonn.de>
2019-03-16 12:02:50 +0100

    button hinzugefügt für später...

--- common/nav.php
+++ common/nav.php
@@ -2,16 +2,17 @@
     <h1><?php echo $commonStrings['WebSiteName']; ?></h1>
 </div>
 <div class="w3-bar w3-teal">
-  <a href="/MVD" class="w3-bar-item w3-button w3-mobile<?php getPage('home');?>">Home</a>
-  <a href="termine.php" class="w3-bar-item w3-button w3-mobile<?php getPage('termine');?>">Termine</a>
-  <a href="meldungen.php" class="w3-bar-item w3-button w3-mobile<?php getPage('meldungen');?>">Meldungen</a>
-  <div class="w3-dropdown-hover w3-mobile">
-    <button class="w3-button w3-mobile w3-blue-gray">Admin</button>
-    <div class="w3-dropdown-content w3-bar-block w3-card-4 w3-gray w3-mobile">
-     <a href="musiker.php" class="w3-bar-item w3-button w3-blue-gray w3-mobile<?php getPage('musiker');?>">Musiker</a>
-      <a href="new-musiker.php" class="w3-bar-item w3-button w3-blue-gray w3-mobile<?php getPage('newmusiker');?>">neuen Musiker anlegen</a>
-      <a href="new-termin.php" class="w3-bar-item w3-button w3-blue-gray w3-mobile<?php getPage('newtermin');?>">neuen Termin erstellen</a>
-    </div>
+    <a href="/MVD" class="w3-bar-item w3-button w3-mobile<?php getPage('home');?>">Home</a>
+    <a href="termine.php" class="w3-bar-item w3-button w3-mobile<?php getPage('termine');?>">Termine</a>
+    <a href="meldungen.php" class="w3-bar-item w3-button w3-mobile<?php getPage('meldungen');?>">Meldungen</a>
+    <div class="w3-dropdown-hover w3-mobile">
+	<button class="w3-button w3-mobile w3-blue-gray">Admin</button>
+	<div class="w3-dropdown-content w3-bar-block w3-card-4 w3-gray w3-mobile">
+	    <a href="musiker.php" class="w3-bar-item w3-button w3-blue-gray w3-mobile<?php getPage('musiker');?>">Musiker</a>
+	    <a href="new-musiker.php" class="w3-bar-item w3-button w3-blue-gray w3-mobile<?php getPage('newmusiker');?>">neuen Musiker anlegen</a>
+	    <a href="new-termin.php" class="w3-bar-item w3-button w3-blue-gray w3-mobile<?php getPage('newtermin');?>">neuen Termin erstellen</a>
+	    <a href="#" class="w3-bar-item w3-button w3-blue-gray w3-mobile<?php getPage('newtermin');?>">Email versenden</a>
+	</div>
   </div>
   <a href="logout.php" class="w3-bar-item w3-button w3-gray w3-mobile">Logout</a>
 </div>
