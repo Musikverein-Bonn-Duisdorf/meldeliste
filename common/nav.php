62eb1656afc41e05c22bac63104eb6a2f5e7c247
Manuel Schedler <schedler@physik.uni-bonn.de>
2019-03-16 12:04:57 +0100

    string geändert

--- common/nav.php
+++ common/nav.php
@@ -8,7 +8,7 @@
     <div class="w3-dropdown-hover w3-mobile">
 	<button class="w3-button w3-mobile w3-blue-gray">Admin</button>
 	<div class="w3-dropdown-content w3-bar-block w3-card-4 w3-gray w3-mobile">
-	    <a href="musiker.php" class="w3-bar-item w3-button w3-blue-gray w3-mobile<?php getPage('musiker');?>">Musiker</a>
+	    <a href="musiker.php" class="w3-bar-item w3-button w3-blue-gray w3-mobile<?php getPage('musiker');?>">Musikerliste</a>
 	    <a href="new-musiker.php" class="w3-bar-item w3-button w3-blue-gray w3-mobile<?php getPage('newmusiker');?>">neuen Musiker anlegen</a>
 	    <a href="new-termin.php" class="w3-bar-item w3-button w3-blue-gray w3-mobile<?php getPage('newtermin');?>">neuen Termin erstellen</a>
 	    <a href="#" class="w3-bar-item w3-button w3-blue-gray w3-mobile<?php getPage('newtermin');?>">Email versenden</a>
