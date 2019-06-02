37d4fe1e10065c0c0baf7b4da116a17cf2a4a946
schedler <schedler@physik.uni-bonn.de>
2019-06-02 22:48:05 +0200

    übersicht mitglieder und nicht-mitglieder

---
+++ mitglied.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+$_SESSION['page']='mitglied';
+include "common/header.php";
+
+if($_SESSION['admin']) {
+$sql = 'SELECT COUNT(`Index`) AS `Count` FROM `MVD`.`User` WHERE `Mitglied` = 1;';
+$dbr = mysqli_query($conn, $sql);
+$row = mysqli_fetch_array($dbr);
+$nMusiker = $row['Count'];
+?>
+<div class="w3-container w3-dark-gray">
+    <h2>Liste aller aktiven Mitglieder (<?php echo $nMusiker; ?>)</h2>
+</div>
+<?php
+$sql = 'SELECT `Index` FROM `MVD`.`User` WHERE `Mitglied` = 1 ORDER BY `Nachname`, `Vorname`;';
+$dbr = mysqli_query($conn, $sql);
+while($row = mysqli_fetch_array($dbr)) {
+    $M = new User;
+    $M->load_by_id($row['Index']);
+    $M->printTableLine();
+}
+?>
+<?php }
+else {
+?>
+    <meta http-equiv="refresh" content="0; URL=index.php" />
+<?php
+}
+include "common/footer.php";
+?>
