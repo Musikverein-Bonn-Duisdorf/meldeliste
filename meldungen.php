1694b02f9b724f7f37f57a09f56086e0ce7ac9c4
schedler <schedler@physik.uni-bonn.de>
2019-06-10 23:28:30 +0200

    bessere darstellung rückmeldungen

--- meldungen.php
+++ meldungen.php
@@ -7,6 +7,9 @@ include "common/header.php";
 <div class="w3-container <?php echo $GLOBALS['commonColors']['titlebar']; ?>">
 <h2>Rückmeldungen</h2>
 </div>
+      <div class="w3-row">
+               <div class="w3-panel w3-col l3 w3-left"></div>
+                        <div class="w3-col l6 s12 m12">
 <?php
 $now = date("Y-m-d");
 $sql = sprintf('SELECT `Index` FROM `Termine` WHERE `Datum` >= "%s" ORDER BY `Datum`, `Uhrzeit`;',
@@ -19,6 +22,9 @@ while($row = mysqli_fetch_array($dbr)) {
     echo $M->printResponseLine();
 }
 ?>
+</div>
+<div class="w3-col l3"></div>
+</div>
 <?php
 include "common/footer.php";
 ?>
