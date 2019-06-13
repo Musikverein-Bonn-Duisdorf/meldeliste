0b9236d64627129f1b8b3630d5d17a176eddf117
Manuel Schedler <manuel@manuelschedler.de>
2019-06-13 14:00:18 +0200

    changed email prefix

--- common/config.php
+++ common/config.php
@@ -13,19 +13,8 @@ $mailconfig = array(
     'from' => "meldeliste@musikverein-bonn-duisdorf.de",
     'fromName' => "Musikverein Duisdorf",
     'secure' => "tls",
-    'subjectprefix' => '[MVD] ',
+    'subjectprefix' => '[MVD-Meldeliste] ',
 );
-
-# $mailconfig = array(
-#     'server' => "mail.gmx.net",
-#     'user' => "manuel.schedler@gmx.de",
-#     'password' => "U,e-Ir.c{F3wi]<QOO[]",
-#     'port' => 465,
-#     'from' => "manuel.schedler@gmx.de",
-#     'fromName' => "Manuel Schedler",
-#     'secure' => "ssl",
-#     'subjectprefix' => '[MVD] ',
-# );
 
 $conn = mysqli_connect($sql['server'], $sql['user'], $sql['password']) or die (mysqli_error($conn));
 
