c484491256e94b6477ddc989833c41f392ce166b
schedler <schedler@physik.uni-bonn.de>
2019-07-02 23:51:33 +0200

    short title für small devices

--- common/site.php
+++ common/site.php
@@ -1,6 +1,7 @@
 <?php
 $site = array(
     'WebSiteName' => "Meldeliste - Musikverein Bonn-Duisdorf",
+    'WebSiteNameShort' => "Meldeliste - MVD",
     'WebSiteURL' => "https://meldeliste.musikverein-bonn-duisdorf.de",
     'MasterPage' => "https://www.musikverein-bonn-duisdorf.de",
     'AdminEmail' => "meldeliste@musikverein-bonn-duisdorf.de",
