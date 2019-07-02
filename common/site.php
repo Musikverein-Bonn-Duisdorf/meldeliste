a6df5ef9ab90a1fb89f600360b9e42c0e8516978
schedler <schedler@physik.uni-bonn.de>
2019-07-02 23:47:52 +0200

    korrigiert masterpage

--- common/site.php
+++ common/site.php
@@ -1,10 +1,10 @@
 <?php
 $site = array(
-    'WebSiteName' => "Meldeliste Musikverein Bonn-Duisdorf",
+    'WebSiteName' => "Meldeliste - Musikverein Bonn-Duisdorf",
     'WebSiteURL' => "https://meldeliste.musikverein-bonn-duisdorf.de",
-    'MasterPage' => "https://musikverein-bonn-duisdorf.de",
+    'MasterPage' => "https://www.musikverein-bonn-duisdorf.de",
     'AdminEmail' => "meldeliste@musikverein-bonn-duisdorf.de",
-    'favicon' => "https://musikverein-bonn-duisdorf.de/favicon.ico",
+    'favicon' => "https://www.musikverein-bonn-duisdorf.de/favicon.ico",
 );
 global $site;
 ?>
