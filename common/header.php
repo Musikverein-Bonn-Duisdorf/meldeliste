582b4623000cb1900b3ed16a4df224c6d669bcf1
Manuel Schedler <manuel@manuelschedler.de>
2019-07-09 20:55:09 +0200

    icons for nav

--- common/header.php
+++ common/header.php
@@ -6,7 +6,9 @@
       <link rel="stylesheet" href="styles/w3.css">
       <link rel="stylesheet" href="styles/w3-colors-highway.css">
       <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-      <?php
+	  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+      <script src="https://use.fontawesome.com/914ad24785.js"></script>
+	  <?php
           include 'include.php';
       ?>
       <link rel="icon" href="<?php echo $GLOBALS['site']['favicon']; ?>" type="image/x-icon">
