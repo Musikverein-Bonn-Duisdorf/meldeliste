524a66deacd58f775c605c84ca4f648aad384509
Manuel Schedler <manuel@manuelschedler.de>
2019-07-10 14:19:32 +0200

    changed order in config

--- common/config.php
+++ common/config.php
@@ -20,6 +20,7 @@ $mailconfig = array(
 );
 
 $conn = mysqli_connect($sql['server'], $sql['user'], $sql['password']) or die (mysqli_error($conn));
+global $conn;
 mysqli_select_db($GLOBALS['conn'], $sql['database']) or die(mysqli_error($conn));
 
 $cronID = '2955bf5d-2014-4c0e-9c52-5ab9a932b4b7';
@@ -27,7 +28,6 @@ $cronID = '2955bf5d-2014-4c0e-9c52-5ab9a932b4b7';
 $googlemapsapi = "AIzaSyDUGmxw5Z7xlPHOtxZyGns028rKUGLtLFA";
 
 global $mailconfig;
-global $conn;
 global $cronID;
 global $dbprefix;
 global $googlemapsapi;
