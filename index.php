b88a54086758d532d0f9b0a53fef89d834691b6e
Manuel Schedler <manuel@manuelschedler.de>
2019-11-12 13:40:13 +0100

    nice button on login page

--- index.php
+++ index.php
@@ -37,11 +37,18 @@ while($row = mysqli_fetch_array($dbr)) {
 if($GLOBALS['optionsDB']['showAppmntPage']) {
     $more = new div;
     $more->tag="a";
-    $more->class="w3-btn w3-mobile w3-border w3-border-black";
+    $more->class="w3-btn w3-hide-large w3-mobile w3-border w3-border-black";
     $more->class=$GLOBALS['optionsDB']['colorBtnSubmit'];
     $more->href="termine.php";
     $more->body="mehr Termine";
     echo $more->print();
+    $moreL = new div;
+    $moreL->tag="a";
+    $moreL->class="w3-btn w3-hide-small w3-hide-medium w3-margin-left w3-padding w3-border w3-border-black";
+    $moreL->class=$GLOBALS['optionsDB']['colorBtnSubmit'];
+    $moreL->href="termine.php";
+    $moreL->body="mehr Termine";
+    echo $moreL->print();
 }
 ?>
 <?php
