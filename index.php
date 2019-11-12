e9b35afae2010506277af9b3e9c6f25d891a14c2
Manuel Schedler <manuel@manuelschedler.de>
2019-11-12 13:43:26 +0100

    bugfix

--- index.php
+++ index.php
@@ -37,14 +37,14 @@ while($row = mysqli_fetch_array($dbr)) {
 if($GLOBALS['optionsDB']['showAppmntPage']) {
     $more = new div;
     $more->tag="a";
-    $more->class="w3-btn w3-hide-large w3-mobile w3-border w3-border-black";
+    $more->class="w3-btn w3-hide-large w3-mobile w3-border w3-border-black w3-margin-bottom";
     $more->class=$GLOBALS['optionsDB']['colorBtnSubmit'];
     $more->href="termine.php";
     $more->body="mehr Termine";
     echo $more->print();
     $moreL = new div;
     $moreL->tag="a";
-    $moreL->class="w3-btn w3-hide-small w3-hide-medium w3-margin-left w3-padding w3-border w3-border-black";
+    $moreL->class="w3-btn w3-hide-small w3-hide-medium w3-margin-left w3-margin-bottom w3-padding w3-border w3-border-black";
     $moreL->class=$GLOBALS['optionsDB']['colorBtnSubmit'];
     $moreL->href="termine.php";
     $moreL->body="mehr Termine";
