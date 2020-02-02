5516f7b165b794219e3f131b659fb37512d0ebbb
Manuel Schedler <manuel@manuelschedler.de>
2020-02-02 14:45:39 +0100

    dev shift meldung

--- meldungen.php
+++ meldungen.php
@@ -7,11 +7,11 @@ requireAdmin();
 
 ?>
 <div class="w3-container <?php echo $GLOBALS['optionsDB']['colorTitleBar']; ?>">
-<h2>Rückmeldungen</h2>
+  <h2>Rückmeldungen</h2>
 </div>
-      <div class="w3-row">
-               <div class="w3-panel w3-col l3 w3-left"></div>
-                        <div class="w3-col l6 s12 m12">
+<div class="w3-row">
+  <div class="w3-panel w3-col l3 w3-left"></div>
+  <div class="w3-col l6 s12 m12">
 <?php
 $now = date("Y-m-d");
 $sql = sprintf('SELECT `Index` FROM `%sTermine` WHERE `Datum` >= "%s" ORDER BY `Datum`, `Uhrzeit`;',
@@ -26,8 +26,8 @@ while($row = mysqli_fetch_array($dbr)) {
     echo $M->printResponseLine();
 }
 ?>
-</div>
-<div class="w3-col l3"></div>
+  </div>
+  <div class="w3-col l3"></div>
 </div>
 <?php
 include "common/footer.php";
