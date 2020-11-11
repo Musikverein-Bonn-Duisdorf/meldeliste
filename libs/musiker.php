e238bd8b02c74e05b5002e9b8ee330ba72715cd9
Manuel Schedler <manuel@manuelschedler.de>
2020-11-11 13:56:39 +0100

    anonymize user on delete command

--- libs/musiker.php
+++ libs/musiker.php
@@ -287,7 +287,7 @@ class User
     }
     public function delete() {
         if(!$this->Index) return false;
-        $sql = sprintf('UPDATE `%sUser` SET `Deleted` = 1, `DeletedOn` = CURRENT_TIMESTAMP WHERE `Index` = "%d";',
+        $sql = sprintf('UPDATE `%sUser` SET `Deleted` = 1, `DeletedOn` = CURRENT_TIMESTAMP, `Vorname` = "gel&ouml;schter", `Nachname` = "Benutzer" WHERE `Index` = "%d";',
         $GLOBALS['dbprefix'],
         $this->Index
         );
