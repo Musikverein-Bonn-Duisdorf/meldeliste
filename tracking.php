470f711ab47e787c6ab1eb65018bbff3ef37725c
Manuel Schedler <manuel@manuelschedler.de>
2021-11-05 11:40:10 +0100

    display germanDate for tracking appmnts

--- tracking.php
+++ tracking.php
@@ -10,7 +10,7 @@ $termin->load_by_id($_POST['termin']);
 <script src="js/track.js"></script>
 <script src="js/meldeshift.js"></script>
 <div class="w3-container <?php echo $GLOBALS['optionsDB']['colorTitleBar']; ?>">
-         <h2>Anwesenheitsliste - <?php echo $termin->Name." (".$termin->Datum.")"; ?></h2>
+         <h2>Anwesenheitsliste - <?php echo $termin->Name." (".germanDate($termin->Datum, 1).")"; ?></h2>
 </div>
 <?php
 echo $termin->TrackingTable();
