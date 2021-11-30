dbf092ea6f90e36e64edeccbe3eea095f20cfd03
Manuel Schedler <manuel@manuelschedler.de>
2021-11-30 21:08:17 +0100

    implemented column insert to SQL

--- libs/SQLtable.php
+++ libs/SQLtable.php
@@ -2,10 +2,35 @@
 class SQLtable
 {
     private $_data = array('Name' => null, 'struct' => null);
+    
+    public function __get($key) {
+        switch($key) {
+	    case 'Index':
+	    case 'Name':
+            return $this->_data[$key];
+            break;
+        default:
+            break;
+        }
+    }
+    public function __set($key, $val) {
+        switch($key) {
+	    case 'Index':
+	    case 'Name':
+            $this->_data[$key] = htmlentities(trim($val));
+            break;
+        default:
+            $this->_data[$key] = $val;
+            break;
+        }	
+    }
+    
+    public function __construct($tableName) {
+        $this->Name = $GLOBALS['dbprefix'].$tableName;
+    }
     public function getStruct() {
         if(!$this->Name) return;
-        $sql = sprintf("SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '%s%s';",
-        $GLOBALS['dbprefix'],
+        $sql = sprintf("SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '%s';",
         $this->Name
         );
         $dbr = mysqli_query($GLOBALS['conn'], $sql);
@@ -13,8 +38,7 @@ class SQLtable
         while($row = mysqli_fetch_array($dbr)) {}
     }
     public function exists() {
-        $sql = sprintf("SELECT object_id FROM sys.tables WHERE name = '%s%s';",
-        $GLOBALS['dbprefix'],
+        $sql = sprintf("SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE' AND TABLE_NAME = '%s';",
         $this->Name
         );
         $dbr = mysqli_query($GLOBALS['conn'], $sql);
@@ -25,29 +49,70 @@ class SQLtable
         return false;
     }
     public function create() {
-        if($this->exists()) return;
-        $sql = sprintf("CREATE TABLE `%s%s` ( `Index` INT NOT NULL AUTO_INCREMENT , PRIMARY KEY (`Index`)) ENGINE = InnoDB;",
-        $GLOBALS['dbprefix'],
+        if($this->exists()) return 1;
+        $sql = sprintf("CREATE TABLE `%s` ( `Index` INT NOT NULL AUTO_INCREMENT , PRIMARY KEY (`Index`)) ENGINE = InnoDB;",
         $this->Name
         );
         $dbr = mysqli_query($GLOBALS['conn'], $sql);
-        sqlerror();        
+        sqlerror();
+        if($this->exists()) return 0;
+        else return -1;
+    }
+    public function createString() {
+        switch($this->create()) {
+        case 0:
+            echo "TABLE '$this->Name' created successfully.";
+            break;
+        case 1:
+            echo "TABLE '$this->Name' already exists.";
+            break;
+        case -1:
+            echo "TABLE '$this->Name' could not be created.";
+            break;
+        default:
+            break;
+        }
     }
     public function delete() {}
     public function columnExists($columnName) {
-        
+        $sql = sprintf("SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = '%s' AND column_name = '%s'",
+        $this->Name,
+        $columnName
+        );
+        $dbr = mysqli_query($GLOBALS['conn'], $sql);
+        sqlerror();
+        while($row = mysqli_fetch_array($dbr)) {
+            return true;
+        }
+        return false;
     }
-    public function addColumn($columnName, $type, $null) {
-        if($this->columnExistS($columnName)) return;
-        $sql = sprintf("ALTER TABLE `%s%s` ADD `%s` %s %s AFTER `Index`;",
-        $GLOBALS['dbprefix'],
+    public function createColumn($columnName, $type, $null) {
+        if($this->columnExistS($columnName)) return 1;
+        $sql = sprintf("ALTER TABLE `%s` ADD `%s` %s %s AFTER `Index`;",
         $this->Name,
         $columnName,
         $type,
         $null
         );
         $dbr = mysqli_query($GLOBALS['conn'], $sql);
-        sqlerror();                
+        sqlerror();
+        if($this->exists()) return 0;
+        else return -1;
+    }
+    public function createColumnString($columnName, $type, $null) {
+        switch($this->createColumn($columnName, $type, $null)) {
+        case 0:
+            echo "TABLE '$columnName' created successfully.";
+            break;
+        case 1:
+            echo "TABLE '$columnName' already exists.";
+            break;
+        case -1:
+            echo "TABLE '$columnName' could not be created.";
+            break;
+        default:
+            break;
+        }
     }
     public function deleteColumn($columnName) {}
 };
