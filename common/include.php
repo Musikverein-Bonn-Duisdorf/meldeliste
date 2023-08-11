16558dcd8f885e3a93ecd39e1c753f384233e468
Manuel Schedler <manuel@manuelschedler.de>
2023-08-11 12:41:53 +0200

    renamed musiker.php to user.php

--- common/include.php
+++ common/include.php
@@ -7,7 +7,7 @@ global $optionsDB;
 include "version.php";
 include "libs/log.php";
 include "libs/div.php";
-include "libs/musiker.php";
+include "libs/user.php";
 include "libs/termin.php";
 include "libs/shift.php";
 include "libs/meldung.php";
