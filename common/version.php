2a8ba87f58858dbd211a907d2aa0f85c309ffd1c
Manuel Schedler <manuel@manuelschedler.de>
2023-09-15 16:40:46 +0200

    release 2023-09-15-57ea4

--- common/version.php
+++ common/version.php
@@ -1,8 +1,8 @@
 <?php
 $version = array(
-	 'String' => "2023-08-04-8b500",
-	 'Date' => "2023-08-04",
-	 'Hash' => "b3e1b44597dbc5b26865c1bf2dcd1d255738b500"
+	 'String' => "2023-09-15-57ea4",
+	 'Date' => "2023-09-15",
+	 'Hash' => "a6e02ceb6365a75b37eb97fb51c7730c4e357ea4"
 );
 
 global $version;
