2aefb7b7cb467722e6b2b9ef7951daa6fcc712af
Manuel Schedler <git@manuelschedler.de>
2023-09-20 02:02:53 +0200

    release 2023-09-20-88279

--- common/version.php
+++ common/version.php
@@ -1,8 +1,8 @@
 <?php
 $version = array(
-	 'String' => "2023-09-20-47d02",
+	 'String' => "2023-09-20-88279",
 	 'Date' => "2023-09-20",
-	 'Hash' => "d94829311881cf4dae79a42d450d629f8b747d02"
+	 'Hash' => "c0910742e23ba6c4e90b68c7d46de9035c388279"
 );
 
 global $version;
