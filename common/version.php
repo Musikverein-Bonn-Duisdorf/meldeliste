dd4a8e35c00535e163e5602ae393b0db43c4eb35
Manuel Schedler <manuel@manuelschedler.de>
2023-09-21 08:58:58 +0200

    release 2023-09-21-ccb26

--- common/version.php
+++ common/version.php
@@ -1,8 +1,8 @@
 <?php
 $version = array(
-	 'String' => "2023-09-20-88279",
-	 'Date' => "2023-09-20",
-	 'Hash' => "c0910742e23ba6c4e90b68c7d46de9035c388279"
+	 'String' => "2023-09-21-ccb26",
+	 'Date' => "2023-09-21",
+	 'Hash' => "27522db74bb48341f16caae270656579093ccb26"
 );
 
 global $version;
