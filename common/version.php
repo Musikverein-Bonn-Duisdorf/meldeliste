ebe94f9f537ee91bd9c2cae831dea6512af2180c
Manuel Schedler <m.schedler@senec.com>
2023-10-06 11:48:46 +0200

    release 2023-10-06-39480

--- common/version.php
+++ common/version.php
@@ -1,8 +1,8 @@
 <?php
 $version = array(
-	 'String' => "2023-10-06-d18d2",
+	 'String' => "2023-10-06-39480",
 	 'Date' => "2023-10-06",
-	 'Hash' => "581602e6bf0db5c158146e4df899a65dd96d18d2"
+	 'Hash' => "595f4c2a1f36b1009c3b89e29d9036a77d439480"
 );
 
 global $version;
