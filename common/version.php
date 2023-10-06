587b24ce145f1457485cf7210a1b312a4b6c77ff
Manuel Schedler <manuel@manuelschedler.de>
2023-10-06 10:44:04 +0200

    release 2023-10-06-39781

--- common/version.php
+++ common/version.php
@@ -1,8 +1,8 @@
 <?php
 $version = array(
-	 'String' => "2023-09-21-b46f8",
-	 'Date' => "2023-09-21",
-	 'Hash' => "184323f11f0ea9446a320e3bb7baa7ad314b46f8"
+	 'String' => "2023-10-06-39781",
+	 'Date' => "2023-10-06",
+	 'Hash' => "dab33a9f267e4d8873976d7c0f317fe051039781"
 );
 
 global $version;
