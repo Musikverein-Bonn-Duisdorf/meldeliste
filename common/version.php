6cc1d84c98d704601008ec4cf0fb6e63f92ac84b
Manuel Schedler <git@manuelschedler.de>
2024-10-23 16:55:26 +0200

    release 2024-10-23-e7ec2

--- common/version.php
+++ common/version.php
@@ -1,8 +1,8 @@
 <?php
 $version = array(
-	 'String' => "2024-03-15-a299d",
-	 'Date' => "2024-03-15",
-	 'Hash' => "01c58746cbcc222ad1f2696b4649a004c65a299d"
+	 'String' => "2024-10-23-e7ec2",
+	 'Date' => "2024-10-23",
+	 'Hash' => "24af9cc42039095c2b4ebf16b226eee1d57e7ec2"
 );
 
 global $version;
