787d68482760399761b08763a8ab86042661ec1f
Manuel Schedler <manuel@manuelschedler.de>
2024-11-12 16:40:10 +0100

    release 2024-11-12-a8e43

--- common/version.php
+++ common/version.php
@@ -1,8 +1,8 @@
 <?php
 $version = array(
-	 'String' => "2024-10-23-eadc2",
-	 'Date' => "2024-10-23",
-	 'Hash' => "a630931eec13c83b3ae6c6fb2613bbaf087eadc2"
+	 'String' => "2024-11-12-a8e43",
+	 'Date' => "2024-11-12",
+	 'Hash' => "3d7980d67b3ac221124c2d65a25673b9b99a8e43"
 );
 
 global $version;
