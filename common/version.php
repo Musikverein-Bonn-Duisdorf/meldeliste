d05b544cb3937427bd8b4013db6124f2924cad2e
Manuel Schedler <manuel@manuelschedler.de>
2024-12-17 09:39:22 +0100

    release 2024-12-17-d47c6

--- common/version.php
+++ common/version.php
@@ -1,8 +1,8 @@
 <?php
 $version = array(
-	 'String' => "2024-11-20-6241c",
-	 'Date' => "2024-11-20",
-	 'Hash' => "e2337a2f41c8351cc771e652cff7c0f5db26241c"
+	 'String' => "2024-12-17-d47c6",
+	 'Date' => "2024-12-17",
+	 'Hash' => "af34e268f8f982b90893be2f32049ba26a4d47c6"
 );
 
 global $version;
