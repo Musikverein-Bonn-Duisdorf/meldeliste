bbdad83f1bca4fe1c758252a7b7c728bc666ab1e
Manuel Schedler <git@manuelschedler.de>
2025-09-07 10:42:40 +0200

    changed links to buttons

--- media.php
+++ media.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$_SESSION['page']='help';
+$_Session['Page']='help';
 $_SESSION['adminpage']=false;
 include "common/header.php";
 ?>
@@ -9,11 +9,17 @@ include "common/header.php";
 </div>
 <div class="w3-container w3-margin-top">
 Hier findest du Konzertaufnahmen, Fotos und weitere Medien für den internen Gebrauch:
-  <ul>
-    <li><a href="https://www.youtube.com/playlist?list=PLcnXPttCC4m4ZrvzYxJf4_tTIBwRjhDDo"><i class="fa-brands fa-youtube"></i> Youtube-Kanal</a></li>
-    <li><a href="https://cloud.musikverein-bonn-duisdorf.de/index.php/s/CorLLnP3Znnxd8E"><i class="fas fa-cloud"></i> Fotos</a></li>
-    <li><a href="https://cloud.musikverein-bonn-duisdorf.de/index.php/s/3nz6GTtJWCLESfx"><i class="fas fa-cloud"></i> Videos</a></li>
-  </ul>
+  <div class="w3-container">
+    <form class="w3-container w3-row" action="https://www.youtube.com/playlist?list=PLcnXPttCC4m4ZrvzYxJf4_tTIBwRjhDDo" target="_blank" method="POST">
+	<button class="w3-btn w3-border w3-margin-top w3-border-black s12 m12 l12 <?php echo $GLOBALS['optionsDB']['colorBtnEdit'] ;?>" type="submit"><i class="fa-brands fa-youtube"></i> Youtube-Kanal</button>
+    </form>
+    <form class="w3-container w3-row" action="https://cloud.musikverein-bonn-duisdorf.de/index.php/s/CorLLnP3Znnxd8E" target="_blank" method="POST">
+	<button class="w3-btn w3-border w3-margin-top w3-border-black s12 m12 l12 <?php echo $GLOBALS['optionsDB']['colorBtnEdit'] ;?>" type="submit"><i class="fas fa-cloud"></i> Fotos</button>
+    </form>
+    <form class="w3-container w3-row" action="https://cloud.musikverein-bonn-duisdorf.de/index.php/s/3nz6GTtJWCLESfx" target="_blank" method="POST">
+	<button class="w3-btn w3-border w3-margin-top w3-border-black s12 m12 l12 <?php echo $GLOBALS['optionsDB']['colorBtnEdit'] ;?>" type="submit"><i class="fas fa-cloud"></i> Videos</button>
+    </form>
+  </div>
 </div>
 <?php
 include "common/footer.php";
