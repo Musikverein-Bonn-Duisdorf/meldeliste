b08e0402e9a493dcd5d992f10ae2e260b845e79f
Manuel Schedler <git@manuelschedler.de>
2025-09-07 11:01:34 +0200

    chaning layout

--- media.php
+++ media.php
@@ -11,13 +11,13 @@ include "common/header.php";
 <h3>Hier findest du Konzertaufnahmen, Fotos und weitere Medien für den internen Gebrauch</h3>
 </div>
   <div class="w3-container w3-row-padding">
-    <form class="w3-col s12 m6 l4" action="https://www.youtube.com/playlist?list=PLcnXPttCC4m4ZrvzYxJf4_tTIBwRjhDDo" target="_blank">
+    <form class="w3-col s12 m6 l4 w3-margin-bottom" action="https://www.youtube.com/playlist?list=PLcnXPttCC4m4ZrvzYxJf4_tTIBwRjhDDo" target="_blank">
 	<button class="w3-btn w3-border w3-border-black w3-block <?php echo $GLOBALS['optionsDB']['colorBtnSubmit'] ;?>" type="submit"><i class="fa-brands fa-youtube"></i> Youtube-Kanal</button>
     </form>
-    <form class="w3-col s12 m6 l4" action="https://cloud.musikverein-bonn-duisdorf.de/index.php/s/CorLLnP3Znnxd8E" target="_blank">
+    <form class="w3-col s12 m6 l4 w3-margin-bottom" action="https://cloud.musikverein-bonn-duisdorf.de/index.php/s/CorLLnP3Znnxd8E" target="_blank">
 	<button class="w3-btn w3-border w3-border-black w3-block <?php echo $GLOBALS['optionsDB']['colorBtnSubmit'] ;?>" type="submit"><i class="fas fa-cloud"></i> Fotos</button>
     </form>
-    <form class="w3-col s12 m6 l4" action="https://cloud.musikverein-bonn-duisdorf.de/index.php/s/3nz6GTtJWCLESfx" target="_blank">
+    <form class="w3-col s12 m6 l4 w3-margin-bottom" action="https://cloud.musikverein-bonn-duisdorf.de/index.php/s/3nz6GTtJWCLESfx" target="_blank">
 	<button class="w3-btn w3-border w3-border-black w3-block <?php echo $GLOBALS['optionsDB']['colorBtnSubmit'] ;?>" type="submit"><i class="fas fa-cloud"></i> Videos</button>
     </form>
   </div>
