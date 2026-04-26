5dbf16925eaf08a0df629c6808b4cbfcacfa990b
Manuel Schedler <git@manuelschedler.de>
2026-04-26 12:55:41 +0200

    fix format

--- inventories.php
+++ inventories.php
@@ -88,7 +88,8 @@ if(requirePermission("perm_showInventories")) {
 <div class="w3-row w3-padding w3-border-bottom w3-border-black w3-hide-small w3-hide-medium">
   <div class="w3-col l1 m1 s1 w3-center w3-border-right"><b>Inventarnummer</b></div>
   <div class="w3-col l2 m2 s2 w3-center w3-border-right"><b>Typ</b></div>
-  <div class="w3-col l4 m4 s4 w3-center w3-border-right"><b>Beschreibung</b></div>
+  <div class="w3-col l2 m4 s4 w3-center w3-border-right"><b>Beschreibung</b></div>
+  <div class="w3-col l2 m4 s4 w3-center w3-border-right"><b>Kommentar</b></div>
   <div class="w3-col l1 m1 s1 w3-center w3-border-right"><b>Kaufdatum</b></div>
   <div class="w3-col l1 m1 s1 w3-center w3-border-right"><b>Kaufpreis</b></div>
   <div class="w3-col l3 m2 s2 w3-center w3-border-right"><b>ausgeliehen an</b></div>
