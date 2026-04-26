be0f35c20cd0d7fb76a8e6ddef5305b1a270aaee
Manuel Schedler <git@manuelschedler.de>
2026-04-26 12:08:17 +0200

    fixed formatting

--- inventories.php
+++ inventories.php
@@ -87,7 +87,7 @@ if(requirePermission("perm_showInventories")) {
 </div>
   <div class="w3-row w3-padding w3-border-bottom w3-border-black w3-hide-small w3-hide-medium">
   <div class="w3-col l1 m1 s1 w3-center w3-border-right"><b>Inventarnummer</b></div>
-  <div class="w3-col l1 m2 s2 w3-center w3-border-right"><b>Instrument</b></div>
+  <div class="w3-col l1 m2 s2 w3-center w3-border-right"><b>Typ</b></div>
   <div class="w3-col l1 m1 s1 w3-center w3-border-right"><b>Beschreibung</b></div>
   <div class="w3-col l1 m1 s1 w3-center w3-border-right"><b>Kaufdatum</b></div>
   <div class="w3-col l1 m1 s1 w3-center w3-border-right"><b>Kaufpreis</b></div>
