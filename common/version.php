f5c4897936412ca2bf19e3283eddb9028a71ba45
Manuel Schedler <git@manuelschedler.de>
2026-07-17 00:44:14 +0200

    release 2026-07-17-54f94

--- common/version.php
+++ common/version.php
@@ -1,8 +1,8 @@
 <?php
 $version = array(
-	 'String' => "2026-07-17-fc3ce",
+	 'String' => "2026-07-17-54f94",
 	 'Date' => "2026-07-17",
-	 'Hash' => "92f1c908537dd7afced79fe4d3aa098be70fc3ce"
+	 'Hash' => "633c97cf03dc8765699830d307eba29b86c54f94"
 );
 
 global $version;
